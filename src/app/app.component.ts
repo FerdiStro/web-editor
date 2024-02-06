a6b9a1f0068f2364508b083b0d813182c79f21f9
ferdinands <ferdinand.strobel@infrontfinance.com>
2024-02-06 14:21:10 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import  { TagComponent } from '@ferdinond/web-editor';
 
 @Component({
   selector: 'app-root',
