3304c0201b9f9dda8e5785f056583b9d33baf28d
ferdinands <ferdinand.strobel@infrontfinance.com>
2024-03-19 16:45:54 +0100

    Yaml/Json valid

--- projects/web-editor/src/lib/editbox/editbox.component.ts
+++ projects/web-editor/src/lib/editbox/editbox.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {Component, ElementRef, Input, OnChanges, OnInit, Renderer2, SimpleChanges} from '@angular/core';
+import {NgForOf, NgStyle} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import * as yaml from 'js-yaml';
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 
 @Component({
@@ -9,65 +10,144 @@ import * as yaml from 'js-yaml';
   standalone: true,
   imports: [
     NgForOf,
-    FormsModule
+    FormsModule,
+    NgStyle
   ],
   templateUrl: './editbox.component.html',
   styleUrl: './editbox.component.css'
 })
-export class EditboxComponent implements OnChanges{
+export class EditboxComponent implements  OnChanges{
+
 
 
 
   @Input() text: string = '';
   textLines: string[] = [];
+  lines:number = 0;
+
+  @Input() valid: boolean = false;
 
 
+  editorLines(): number[] {
+    return Array.from({length: this.lines}, (_, index) => index);
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['text'].currentValue && changes['text']) {
-      this.onTextChanged();
+    if(changes['text'] != undefined){
+      if (changes['text'].currentValue && changes['text']) {
+        this.textLines = this.text.split("\n")
+        this.lines = this.text.split("\n").length
+      }
     }
+    this.validOnChange();
   }
 
+  onInput(event: Event) {
 
-  onTextChanged(): void {
-    this.textLines = this.text.split("\n");
+    const editedContent = (event.target as HTMLElement).textContent;
+    this.text = editedContent || '';
 
-      //todo: toggle switch JSON/YAML
+    const test = (event.target as HTMLElement).outerHTML;
 
-      //Json-Valid
+
+
+    this.lines = test.split("</div>").length + test.split("<br>").length - test.split("<br></div>").length - 1;
+
+
+    this.validOnChange();
+
+  }
+
+  textValidStyle =  {
+    'border': '1px solid gray'
+  };
+
+  errorString:string = '';
+  validOnChange(): void {
+    let val = false;
+
+    if(this.valid){
       try {
-        const jsonObject = JSON.parse(this.text);
-        console.log(jsonObject)
+        JSON.parse(this.text);
+        val = true;
 
       } catch (error) {
-        console.log(error)
-      }
-      //Yaml-Valid
+        val = false;
+        // @ts-ignore
+        let test =  error.toString();
+        this.errorString = test
 
-    try {
-      const yamlObject = yaml.load(this.text)
-      console.log(yamlObject);
-    } catch (error) {
-      console.log(error);
+
+        let col = test.split("column")[1].split(")")[0];
+        console.log(col)
+
+
+
+
+
+      }
+    }
+    if(!this.valid){
+      try {
+        yaml.load(this.text)
+        val = true;
+
+
+      }catch (error) {
+        val = false;
+        // @ts-ignore
+        let test =  error.toString();
+        this.errorString = test
+      }
+    }
+
+    if(!val){
+      this.textValidStyle = {
+        'border':'1px solid red'
+      };
+    }else {
+      this.errorString = 'Valid'
+
+
+      this.textValidStyle = {
+        'border':'1px solid grey'
+      };
     }
 
   }
+
+
+
+
+
+
   onKeyDown(event: KeyboardEvent): void {
+    const editedContent = (event.target as HTMLElement).textContent;
+    this.text = editedContent || '';
+
     if (event.key === 'Tab') {
       event.preventDefault();
-      const textarea = event.target as HTMLTextAreaElement;
-      const start = textarea.selectionStart;
-      const end = textarea.selectionEnd;
+      document.execCommand('insertText', false, '\t');
 
-      const tabCharacter = '  ';
-      const tabbedValue = textarea.value.substring(0, start) + tabCharacter + textarea.value.substring(end);
-
-
-      textarea.value = tabbedValue;
-      textarea.setSelectionRange(start + tabCharacter.length, start + tabCharacter.length);
     }
   }
+
+  get renderedTextLines(): string {
+    return this.textLines.map(line => this.escapeHtml(line)).join('<br>'); // Konvertiert Textzeilen in HTML mit <br> für Zeilenumbrüche
+  }
+
+
+
+
+
+  escapeHtml(unsafe: string): string {
+    return unsafe.replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/\t/g, '&emsp;');
+  }
+
+
 
 
 
